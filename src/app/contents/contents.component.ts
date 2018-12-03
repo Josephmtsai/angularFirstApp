4ea61ecb4c373be2b13f92dcca9c26187f24954e
Unknown <mmx362003@gmail.com>
2018-12-04 00:24:37 +0800

    Change contents html

--- src/app/contents/contents.component.ts
+++ src/app/contents/contents.component.ts
@@ -2,18 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'contents-component',
-  templateUrl: './contents.component.html'
+  templateUrl: './contents.component.html',
+  styleUrls: ['./contents.component.scss']
 })
 export class contentsComponent {
-  serverInfo = 'offline';
-  serverList = ['a', 'b'];
-  constructor() {
-    this.serverInfo = Math.random() > 0.5 ? 'online' : 'offline';
-  }
-  getColor() {
-    return this.serverInfo === 'online' ? 'green' : 'yellow';
-  }
-  onAddServer() {
-    this.serverList.push(this.serverInfo);
+  displayDetails = false;
+  serverHistoryList = [];
+  constructor() {}
+
+  onDisplayDetails() {
+    this.displayDetails = !this.displayDetails;
+    this.serverHistoryList.push(this.displayDetails);
   }
 }
